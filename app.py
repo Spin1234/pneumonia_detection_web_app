e24f05b13e1c77fbc20ac7647fd84b6b60955e1e
Sayak Pal <121346103+Spin1234@users.noreply.github.com>
2025-01-18 14:50:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000, threaded=True)
+    app.run(debug=True)
